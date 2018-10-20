cf3eb5efb09d067739961172828d4aeb810f5a00
tangshiyuan <tang.shiyuan@jancar.com.cn>
2018-10-20 16:52:23 +0800

    音量条加减音量修改

--- src/com/android/systemui/volume/VolumeDialog.java
+++ src/com/android/systemui/volume/VolumeDialog.java
@@ -437,7 +437,7 @@ public class VolumeDialog {
                         }
                     }
                 } else {
-                    final boolean vmute = row.ss.level == 0;
+                    final boolean vmute = mController.getStreamMute(stream);
                     mController.setStreamVolume(stream, vmute ? row.lastAudibleLevel : 0);
                     mController.setStreamMute(stream,!vmute);
                     if (row.lastAudibleLevel > 0) {
