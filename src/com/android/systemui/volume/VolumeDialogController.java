40545c90d3cf755dd665fa9a502a0234cf2cb363
tangshiyuan <tang.shiyuan@jancar.com.cn>
2018-10-20 16:14:25 +0800

    音量条加减音量修改

--- src/com/android/systemui/volume/VolumeDialogController.java
+++ src/com/android/systemui/volume/VolumeDialogController.java
@@ -350,8 +350,7 @@ public class VolumeDialogController {
         if (fromKey) {
             if (flags == 4112) {
                 FlyLog.d("set last stream=%d",stream);
-                boolean ismute = mAudio.isStreamMute(stream);
-                if (ismute) {
+                if ((mAudio.getStreamVolume(stream)==0)) {
                     loadLastVolume(stream);
                 }
             }
@@ -911,7 +910,7 @@ public class VolumeDialogController {
                     if (oldLevel != 0) {
                         saveLastVolume("" + stream, oldLevel);
                     }else{
-                        loadLastVolume(stream);
+//                        loadLastVolume(stream);
                     }
                 } else {
                     FlyLog.e("don't care stream=%d, level=%d,oldlevel=%d,get_level=%d",stream,level,oldLevel,get_level);
