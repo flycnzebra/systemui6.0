1d90cf7b68c75ed14fd809d82cbfe9c6137f954a
tangshiyuan <tang.shiyuan@jancar.com.cn>
2018-11-30 14:59:05 +0800

    亮度条

--- src/com/android/systemui/settings/BrightnessController.java
+++ src/com/android/systemui/settings/BrightnessController.java
@@ -183,6 +183,7 @@ public class BrightnessController implements ToggleSlider.Listener {
         mAutomaticAvailable = context.getResources().getBoolean(
                 com.android.internal.R.bool.config_automatic_brightness_available);
         mPower = IPowerManager.Stub.asInterface(ServiceManager.getService("power"));
+        setBrightness(100);
     }
 
     public void addStateChangedCallback(BrightnessStateChangeCallback cb) {
